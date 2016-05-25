3b950545a1b7e0c55adc32b25380bf1fe773246e
megan71 <megan.imperato@greenwichschools.org>
2016-05-25 12:06:27 -0400

    Delete navbar.js

--- navbar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='page1.html'>Membership</a></li>" +
-"<li><a href='page2.html'>Your Profile</a></li>" +
-"</ul>";
