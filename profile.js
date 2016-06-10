e1c2da3ca39a68d495f88e460912d59d961bd0c9
megan71 <megan.imperato@greenwichschools.org>
2016-06-10 14:07:39 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,64 @@
+google.load("visualization", '1', {packages:['corechart']});  //needed for API tables
+google.setOnLoadCallback(readData);  //will call the specified callback function once the document loads
+
+function readData() {
+	var query = new google.visualization.Query( //asking for the data from the Google Spreadsheet
+	'https://docs.google.com/spreadsheets/d/1QZgCOAdhF7HJ9Gmr65V4vgpXBZK-6r_1Y2asfNBDntE/edit?usp=sharing');
+	query.send(handleQueryResponse); //call the response
+}
+		
+function handleQueryResponse(response) { //enacted when the query response is returned
+	if (response.isError()) {
+	alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return; //Alert the user with the error code and return to the place where this function was called
+	}
+
+	var data = response.getDataTable(); //format data
+	var csvData = google.visualization.dataTableToCsv(data); //make csv
+	var data2=csvData.split("\n"); //split by line breaks
+
+	var data3= [];
+	for (i=0; i<data2.length-1; i++){ //gets rid of extra place
+		data3.push(data2[i]);
+	}
+		  
+	for(i=0; i<data3.length; i++){
+		data3[i]= data3[i].split(","); //create 2d array by splitting by commas
+	}
+
+	var members=[]; //array of all member objects
+
+	for (i=0; i<data3.length; i++){
+		makeObject(data3[i][0],data3[i][1],data3[i][2],data3[i][3],data3[i][4]);
+	}
+
+	function makeObject(date1, first, last, email, state) { //object creater
+		var obj = {
+			date: date1,
+			firstName: first,
+			lastName: last,
+			email: email,
+			state: state
+		};
+		members.push(obj); //add to total members array
+	}
+
+	var email= prompt("Once you have registered on the membership page, enter your email address to access your profile page.");
+	var member; //indictes where in the array a gien member is
+
+	function findMember(email){ //searches spreadsheet file for email address and returns object loaction as "member"
+		for (ii=0; ii<members.length; ii++){
+			if (members[ii].email===email){
+				member=ii;
+			}
+		}
+	}
+
+	findMember(email); //call function
+
+	//fills elements on page using object data
+	document.getElementById("name").innerHTML="Name: "+members[member].firstName+" "+members[member].lastName; 
+	document.getElementById("email").innerHTML="Email Address: "+members[member].email;
+	document.getElementById("state").innerHTML="State: "+members[member].state
+	document.getElementById("date").innerHTML="Date & Time Joined: "+members[member].date;
+} //This brace ends the function handleQueryResponse
---
+++ readPostForm.js
@@ -0,0 +1,99 @@
+google.load("visualization", '1', {packages:['corechart']});  //needed for API tables
+google.setOnLoadCallback(readData);  //will call the specified callback function once the document loads
+
+function readData() {
+	var query = new google.visualization.Query( //asking for the data from the Google Spreadsheet
+	'https://docs.google.com/spreadsheets/d/19AeNg1v8PFJqSTDMTI2KkhqiV1-yaNYiuBlLdYkXW1U/edit?usp=sharing');
+  
+	query.send(handleQueryResponse); //call the response
+}
+		
+function handleQueryResponse(response) { //enacted when the query response is returned
+	if (response.isError()) {
+	alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return; //Alert the user with the error code and return to the place where this function was called
+	}
+
+	var data = response.getDataTable(); //format data
+	var csvData = google.visualization.dataTableToCsv(data); //make csv
+	var data2=csvData.split("\n"); //split by line breaks
+
+	var data3= [];
+	for (i=0; i<data2.length-1; i++){ //gets rid of extra place
+		data3.push(data2[i]);
+	}
+		  
+	for(i=0; i<data3.length; i++){
+		data3[i]= data3[i].split(","); //create 2d array by splitting by commas
+	}
+	console.log(data3);
+
+	var posts=[]; //make array to contain all post objects
+
+	for (i=0; i<data3.length; i++){
+		makeObject(data3[i][0],data3[i][1],data3[i][2],data3[i][3],data3[i][4]); //calls object creation given elements in data array
+	}
+
+	console.log(posts);
+	//console.log(posts[0].rating);
+	//makePost();
+
+	function makeObject(date1, title1, rating1, descrition1, image1) { //function to create a post object
+		var obj = {
+			date: date1,
+			showtitle: title1,
+			rating: rating1,
+			showdescription: descrition1,
+			image: image1
+		};
+		posts.push(obj);
+	}
+
+	for (i=0; i<posts.length; i++){
+		makePost(posts[i]); //add to array of all posts
+	}
+
+} //This brace ends the function handleQueryResponse
+	
+
+function makePost(obj){ //makes the post oject into an element on the page
+	var post = document.createElement("DIV"); //create div
+	post.className= "newPost"; //assigning a class allows the css to apply to multiple things
+
+	var center= document.createElement("CENTER"); //centers everything
+
+	var showTitle = document.createElement("H2"); //create heading
+	showTitle.className= "showTitle";
+	var showTitleText = document.createTextNode("Title: "+obj.showtitle);
+
+	var showImg= document.createElement("IMG") //create image
+	showImg.src= obj.image;
+	showImg.className= "showImg";
+
+	var rating= document.createElement("H1") //create heading
+	var ratingText= document.createTextNode("Rating: "+obj.rating+"/10");
+	if(Number(obj.rating)>7){ //allows different formatting depending on high, low, or okay rating
+		rating.className= "ratingGood";
+	} else if (Number(obj.rating)>3){
+		rating.className= "ratingOkay";
+	} else {
+		rating.className= "ratingBad";
+	}
+
+	var description = document.createElement("P"); //create paragraph
+	description.className= "description";
+	var descriptionText = document.createTextNode("Description: "+obj.showdescription);    
+
+	//time to append the children!!
+	showTitle.appendChild(showTitleText); 
+	center.appendChild(showTitle);  
+	center.appendChild(showImg); 
+	rating.appendChild(ratingText);
+	center.appendChild(rating);
+	description.appendChild(descriptionText); 
+	center.appendChild(description);    
+	post.appendChild(center);                          
+	document.getElementById("total").appendChild(post);
+	var br= document.createElement("BR");                                  
+	document.getElementById("total").appendChild(br); 
+}
